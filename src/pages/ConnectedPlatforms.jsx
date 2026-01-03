267f045743b329a5e02a152e9a31f8b8cc238fc7
Vaibhav Kumar ratan <kumarratanvaibhav@gmail.com>
2026-01-03 07:16:09 +0000

    File changes

--- src/pages/ConnectedPlatforms.jsx
+++ src/pages/ConnectedPlatforms.jsx
@@ -47,7 +47,7 @@ const PLATFORMS = [
     scope: "https://www.googleapis.com/auth/yt-analytics.readonly https://www.googleapis.com/auth/youtube.readonly",
     redirectUri: "https://zerithum-copy-36d43903.base44.app/authcallback",
     requiresApiKey: false,
-    clientId: "985180453886-8qbvanuid2ifpdoq84culbg4gta83rbn.apps.googleusercontent.com"
+    clientId: "985180453886-pof8b9qoqf1a7cha901khhg9lk7548b4.apps.googleusercontent.com"
   },
   {
     id: "patreon",
